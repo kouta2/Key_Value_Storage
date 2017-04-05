4e828a2306989de7a313c6c9ed307de363679bee
Drake <ptdrake2@sp17-cs425-g32-05.cs.illinois.edu>
2017-04-05 17:15:42 -0500

    renamed

---
+++ Executor.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Executor extends Thread{
+
+	@Override
+	public void run(){
+
+
+	}	
+
+
+
+	/*
+ *	input line: line of input from user or batch file
+ *	return: array of arguments to command, where args[0] is the name of the command
+ * */
+	private static ArrayList<String> parse(String line){
+
+		ArrayList<String> args = new ArrayList<String>();
+		
+
+		return args;
+	}
+
+
+	//for testing only
+	public static void main(String [] args){
+		line = "SET DOG 2"		
+
+
+
+	}
+
+
+}
