ad29ad5468899abf26f374b79ba4b61df7ce554c
Kouta <kouta2@sp17-cs425-g32-01.cs.illinois.edu>
2017-04-06 15:53:00 -0500

    debugging rpc

--- ConnectToOtherRPCs.java
+++ ConnectToOtherRPCs.java
@@ -1,5 +1,7 @@
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  * attempts to connect to other processes to use their RPCs
@@ -10,11 +12,11 @@ public class ConnectToOtherRPCs implements Runnable
     
     boolean ip_connected_clients[] = {false, false, false, false, false, false, false, false, false, false};
     int port_num = 2001;
-    String ip_addrs [] = {"172.22.146.231", "172.22.146.233", "172.22.146.235", "172.22.146.237", "172.22.146.239", "172.22.146.241", "172.22.146.243", "172.22.146.245", "172.22.146.247", "172.22.146.249"}; // list of ip addresses used for accepting clients and connecting to clients
+    ArrayList<String> ip_addrs = new ArrayList<String> (Arrays.asList("172.22.146.231", "172.22.146.233", "172.22.146.235", "172.22.146.237", "172.22.146.239", "172.22.146.241", "172.22.146.243", "172.22.146.245", "172.22.146.247", "172.22.146.249")); // list of ip addresses used for accepting clients and connecting to clients
     RPCFunctions list_of_client_rpcs [];
-    public ConnectToOtherRPCs(int p_num, RPCFunctions[] client_rpcs, int process)
+    public ConnectToOtherRPCs(int p_num, RPCFunctions client_rpcs [], int process)
     {
-        // ip_addrs = ArrayUtils.removeElement(ip_addrs, ip_addrs[process - 1]);
+        ip_addrs.remove(process - 1);
         port_num = p_num;
         list_of_client_rpcs = client_rpcs;
     }
@@ -24,15 +26,16 @@ public class ConnectToOtherRPCs implements Runnable
         // in the future, we should put this thread to sleep when there are no more clients to connect to and have the failure detector class wake us up when it detects a failure
         while(true)
         {
-            for(int i = 0; i < ip_addrs.length; i++)
+            for(int i = 0; i < ip_addrs.size(); i++)
             {
                 if(!ip_connected_clients[i])
                 {
                     try
                     {
-                        Registry client_sock = LocateRegistry.getRegistry(ip_addrs[i], port_num);
+                        Registry client_sock = LocateRegistry.getRegistry(ip_addrs.get(i), port_num);
                         list_of_client_rpcs[i] = (RPCFunctions) client_sock.lookup("RPCFunctions");
                         ip_connected_clients[i] = true;
+                        System.out.println("Connected to " + ip_addrs.get(i));
                     }
                     catch (Exception e)
                     {
--- main.java
+++ main.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.net.InetAddress;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main implements RPCFunctions {
 
@@ -32,7 +33,7 @@ public class main implements RPCFunctions {
 	static long [] IDS = {0L,429496729L,858993459L,1288490188L,1717986918L,2147483648L,2576980377L,3006477107L,3435973836L,3865470566L};
  	static long ID = IDS[0];
 
-    static RPCFunctions list_of_client_rpcs [] = new RPCFunctions[9]; // list of RPCs to communicate with other machines
+    static RPCFunctions list_of_client_rpcs [] = new RPCFunctions[10]; // list of RPCs to communicate with other machines
     static int port_num = 2001;
 
     static int PROCESS_NUM;
