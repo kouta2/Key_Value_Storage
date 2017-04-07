3fe7eec56e5c59fe512a0309b411d9699e234847
Drake <ptdrake2@sp17-cs425-g32-05.cs.illinois.edu>
2017-04-06 20:47:39 -0500

    routing works

--- Executor.java
+++ Executor.java
@@ -1,13 +1,35 @@
 import java.util.*;
 import java.security.MessageDigest;
 
+
 public class Executor extends Thread{
 
+	
+	static long [] IDS = {0L,429496729L,858993459L,1288490188L,1717986918L,2147483648L,2576980377L,3006477107L,3435973836L,3865470566L};
+	
 	@Override
 	public void run(){
 
 
 	}	
+
+
+	/*
+ *
+ *  function to calculate clockwise mod 2^32 distance from a->b
+ *  */
+	private static long cw_distance(long a, long b){
+		if (b >= a){
+			return b-a;
+		}else{
+			return (b + 4294967296L) - a; 
+		}
+
+	}
+
+
+
+
 
 	/*
  *  Big blackbox function to route, execute, and return output to user
@@ -17,7 +39,7 @@ public class Executor extends Thread{
 	private static ArrayList<String> execute(String line){
 
 		ArrayList<String> args = new ArrayList<String>(Arrays.asList(line.split(" ")));
-		System.out.println(args.toString());
+		//System.out.println(args.toString());
 		
 		//in that array,args[1] is the key (if the command takes one)
 		String key = "";
@@ -25,7 +47,28 @@ public class Executor extends Thread{
 			key = args.get(1);
 		}
 
-		System.out.println(key);
+		if (args.get(0).equals("SET")){
+
+			//RPC business!			
+
+		}else if (args.get(0).equals("GET")){
+
+			//RPC business!
+
+
+		}else if (args.get(0).equals("LIST_LOCAL")){
+	
+			
+	
+		}else if (args.get(0).equals("OWNERS")){
+			
+		}else if (args.get(0).equals("BATCH")){
+
+		}else{
+			System.err.println("ERROR: Unsupported Command");	
+		}
+			
+
 
 		//ROUTE(KEY) (SKIP THIS STEP IF LIST_LOCAL
 		
@@ -44,52 +87,65 @@ public class Executor extends Thread{
  * return: VM that the key belongs to 
  * */
 
-	private static int route(String key){
+	private static long route(String key){
 		/*
  *		From my notes on routing for Chord:
  *			At each step attempt to minimize your clockwise distance to the key
  *			If you can't get any closer without going over, go to your successor, and stop!
  * 		*/
 
-		//need to turn this key into a 256 bit number
-		MessageDigest md = null;
-		try{
-			md = MessageDigest.getInstance("SHA-256");
-		}catch (Exception ex){
-			System.out.println("No such algorithm!");
-		}
+		//need to turn this key into a 32 bit number
+			MessageDigest md = null;
+			try{
+				md = MessageDigest.getInstance("SHA-256");
+			}catch (Exception ex){
+				System.out.println("No such algorithm!");
+			}
 			
-		md.update(key.getBytes());
-		byte byteData[] = md.digest();
-		//just take last 4 bytes
-		byteData = Arrays.copyOfRange(byteData, 12, 16);
+			md.update(key.getBytes());
+			byte byteData[] = md.digest();
+			//just take last 4 bytes
+			byteData = Arrays.copyOfRange(byteData, 12, 16);
 
-		StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < byteData.length; i++) {
-        	sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-        }
-	
-		int k = Integer.parseInt(sb.toString(),16);
-		System.out.println(k);
+			StringBuffer sb = new StringBuffer();
+        	for (int i = 0; i < byteData.length; i++) {
+        		sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        	}	
+		//	System.out.println(sb);	
+			long k = Long.parseLong(sb.toString(),16);
+		//	System.out.println(k);
+		//done, k is that number!
+
 		
-
-		Stabilizer stab = new Stabilizer();
-		long curr_node = main.ID; //starting point
-		long[] finger_table = stab.finger_table(curr_node); //finger_table global from main
+		long curr_node = IDS[3]; //starting point (hard coded for now)
 		boolean done = false;
-	/*
+		long found_id = 0; 	
 		while (!done){
-			//need to find the biggest entry < k, or successor otherwise
-			for (int i = 0; i < 256; i++){
-					
+			//need to find the biggest entry in my table < k, or successor otherwise
 
+			long [] ft = Stabilizer.finger_table(curr_node);
+			long best = curr_node; 
+			boolean found_closer = false; 
+			for (int i = 0; i < 32; i++){
+				if (ft[i] < k && cw_distance(ft[i],k) < cw_distance(best,k)){
+					best = ft[i]; 
+					found_closer = true; 	
+				}
+
+			}			
+	
+			if (found_closer){
+				curr_node = best; 
+			}else{
+				found_id = ft[0]; 
+				done = true; 	
 			}
 
-
-
 		}
-*/
-		return 0;
+
+	
+		System.out.println(found_id);
+		return found_id;
 	}
 
 
@@ -98,7 +154,14 @@ public class Executor extends Thread{
 		String line = "SET DOG 2";
 		ArrayList<String> command = execute(line);		
 		
-		route("Dog");
+		route("dog");
+		route("Course");
+		route("Hero");
+		route("Yeee_boi");
+
+		for (int i = 0; i < 50; i ++){
+			route(Integer.toString(i));
+		}
 
 
 	}
--- Stabilizer.java
+++ Stabilizer.java
@@ -1,6 +1,9 @@
 public class Stabilizer extends Thread{
 
 
+	static long [] IDS = {0L,429496729L,858993459L,1288490188L,1717986918L,2147483648L,2576980377L,3006477107L,3435973836L,3865470566L};
+
+
 	@Override
 	public void run(){
 
@@ -11,21 +14,30 @@ public class Stabilizer extends Thread{
  *param id: id you want the finger table for
  *return: a finger table belonging to the local VM, based on who's living
  * */
-	public long[] finger_table(long id){
+	public static long[] finger_table(long id){
 
 	long []pows = {1L,2L,4L,8L,16L,32L,64L,128L,256L,512L,1024L,2048L,4096L,8192L,16384L,32768L,65536L,131072L,262144L,524288L,1048576L,2097152L,4194304L,8388608L,16777216L,33554432L,67108864L,134217728L,268435456L,536870912L,1073741824L,2147483648L,4294967296L};
 	long[] finger_table = new long[32];
 		//for every entry
 		for (int i = 0; i < 32; i ++){
 			long num = id + pows[i];
-			num = num % (4294967296L-1); //	
+			num = num % (pows[32]); //	
+			
 
 			//just need to find first entry in the VM list that's bigger than this number
-			long [] vms = main.IDS;
+			long [] vms = IDS; //temporary for now! this will have to reference the globals in main!
+			finger_table[i] = 0; 
 			for (int j = 0; j < 10; j ++){
+
+				//edge case, if the num is bigger than the biggest but not big enough to wrap around
+				if (num > IDS[IDS.length - 1]){
+					finger_table[i] = IDS[0];
+					break;
+				}
 				if (vms[j] >= num){
 					finger_table[i] = vms[j];
-				}
+					break; //only take the first one	
+			}
 
 			}
 		}
@@ -33,6 +45,31 @@ public class Stabilizer extends Thread{
 		return finger_table;
 	}
 
+	public static void main(String [] args){
+		
+		int id = 8;
+		System.out.println("ID: " + IDS[id]);
+		long ft[] = finger_table(IDS[id]);		
+		for (int i = 0; i < 32; i ++){
+			System.out.println(i + ": " + ft[i]);
+		}	
+	
 
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
