420b077a90e9972f043f699b27c3cb4dc635a1bc
Kouta <kouta2@sp17-cs425-g32-01.cs.illinois.edu>
2017-04-06 22:00:00 -0500

    actually renamed file

---
+++ AcceptRPCConnections.java
@@ -0,0 +1,34 @@
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+/*
+ * handles the client connections to us to use our RPC calls
+ * 
+ */
+public class AcceptRPCConnections implements Runnable
+{
+    int port_num;
+
+    public AcceptRPCConnections(int p_num)
+    {
+        port_num = p_num;
+    }    
+
+    public void run()
+    {
+        try
+        {
+            main m = new main();
+            RPCFunctions stub = (RPCFunctions) UnicastRemoteObject.exportObject(m, 0);
+            Registry registry = LocateRegistry.getRegistry(port_num);
+            registry.bind("RPCFunctions", stub);
+            System.err.println("Server ready");
+        }
+        catch (Exception e)
+        {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
